2f21d7606c6dacaade2362905767dbfe2d75fa7c
Nur Azis Saputra <nurazissaputra@gmail.com>
2025-01-16 00:10:47 +0700

    fix(uuid): change from increment id to uuid

--- app/Models/Division.php
+++ app/Models/Division.php
@@ -3,8 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 
 class Division extends Model
 {
-    //
+    use HasFactory, HasUuids;
+
+    public $incrementing = false;
 }
